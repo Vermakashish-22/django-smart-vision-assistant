fe0fad02c4903e639e9e0ca95a18c1b5e5a53946
Kashish Verma <vermakashish2922@gmail.com>
2025-08-14 23:29:14 +0530

    Update requirements

--- miniproject/urls.py
+++ miniproject/urls.py
@@ -22,7 +22,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('about/', views.about, name='about'),
     path('services/', views.services, name='services'),
     path('contact/', views.contact, name='contact'),
     path('admin/', admin.site.urls),
